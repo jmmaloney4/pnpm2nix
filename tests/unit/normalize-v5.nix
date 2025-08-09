71ffbc8983baa3d0ded900728a6298dbe5e7a067
Jack Maloney <jmmaloney4@gmail.com>
2025-08-09 11:51:52 -0500

    test: add unit eval tests for normalize-v5 and normalize-v9 IR shape

---
+++ tests/unit/normalize-v5.nix
@@ -0,0 +1,25 @@
+{ }:
+
+let
+  pkgs = import <nixpkgs> {};
+  lib = pkgs.lib;
+  normalize = import ../../lockfile/normalize.nix { inherit pkgs lib; };
+  raw = {
+    lockfileVersion = 5;
+    packages = {
+      "/camelcase/4.1.0" = {};
+    };
+    dependencies = { camelcase = "4.1.0"; };
+  };
+  ir = normalize raw;
+  hasPkg = builtins.hasAttr "/camelcase/4.1.0" ir.packages;
+  constituentsLen = if hasPkg then (builtins.length ir.packages."/camelcase/4.1.0".constituents) else 0;
+  depCamelcase = ir.dependencies.camelcase or null;
+in {
+  lockfileVersionMajor = ir.lockfileVersionMajor;
+  hasPkg = hasPkg;
+  constituentsLen = constituentsLen;
+  depCamelcaseIsString = builtins.isString depCamelcase;
+}
+
+
---
+++ tests/unit/normalize-v9.nix
@@ -0,0 +1,30 @@
+{ }:
+
+let
+  pkgs = import <nixpkgs> {};
+  lib = pkgs.lib;
+  normalize = import ../../lockfile/normalize.nix { inherit pkgs lib; };
+  raw = {
+    lockfileVersion = "9.0";
+    importers = { "." = {
+      dependencies = { leftpad = "1.3.0"; };
+    }; };
+    packages = {
+      "leftpad@1.3.0" = { resolution = { integrity = "sha512-..."; }; };
+    };
+    snapshots = {
+      "leftpad@1.3.0" = { };
+    };
+  };
+  ir = normalize raw;
+  hasPkg = builtins.hasAttr "/leftpad/1.3.0" ir.packages;
+  constituentsLen = if hasPkg then (builtins.length ir.packages."/leftpad/1.3.0".constituents) else 0;
+  depLeftpad = ir.dependencies.leftpad or null;
+in {
+  lockfileVersionMajor = ir.lockfileVersionMajor;
+  hasPkg = hasPkg;
+  constituentsLen = constituentsLen;
+  depLeftpadIsString = builtins.isString depLeftpad;
+}
+
+
